95e569a51b00db98a8f985ab3ecfd0e6e0d04a99
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2024-11-04 22:42:49 -0800

    set up HTML, basic styles, created variables in js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Game Information
+const turnsDisplay = document.getElementById('turns');
+const matchesDisplay = document.getElementById('matches');
+
+// Game board and cards
+const gameBoard = document.querySelector('.game-board');
+const cards = document.querySelectorAll('.card');
+
+// Buttons
+const startButton = document.getElementById('start-btn');
+const endButton = document.getElementById('end-btn');
